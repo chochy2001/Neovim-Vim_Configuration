3db9d953624fe12419aaf0000217d5af169870e7
chochy2001 <ohchochy@gmail.com>
2025-04-20 19:21:57 -0600

    Primera iteracion de nueva implementacion de plugins con neovim usando lazyvim y demas plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,37 +1,15 @@
---Establecer el líder de teclas
-vim.g.mapleader = " "
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
--- Cargar configuraciones
-require("plugins")
-require("settings")
-require("keymaps")
-require("lsp")
-require("plugin.colors")
-require("plugin.harpoon")
-require("plugin.undotree")
-require("mappings")
-
--- vim.cmd("set expandtab") -- Convertir tabulaciones en espacios
--- vim.cmd("set tabstop=2") -- Número de espacios que representa una tabulación
--- vim.cmd("set shiftwidth=2") -- Número de espacios para la indentación
---
--- -- Bootstrap lazy.nv
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not (vim.uv or vim.loop).fs_stat(lazypath) then
--- 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
--- 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
--- 	if vim.v.shell_error ~= 0 then
--- 		vim.api.nvim_echo({
--- 			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
--- 			{ out, "WarningMsg" },
--- 			{ "\nPress any key to exit..." },
--- 		}, true, {})
--- 		vim.fn.getchar()
--- 		os.exit(1)
--- 	end
--- end
--- vim.opt.rtp:prepend(lazypath)
--- local plugins = {}
--- local opts = {}
---
--- require("lazy").setup(plugins, opts)
+require("vim-options")
+require("lazy").setup("plugins")
--- .config/nvim/lua/keymaps/json.lua
+++
@@ -1,3 +0,0 @@
---json lua
-vim.api.nvim_create_user_command('Fjson', 'source ~/.vim/scripts/formatear_json.vim', {})
-
