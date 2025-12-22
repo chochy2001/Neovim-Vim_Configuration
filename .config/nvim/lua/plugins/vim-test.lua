2d957342c91dc6265b39b010061a8fdccc98629c
chochy2001 <ohchochy@gmail.com>
2025-12-22 00:12:07 -0600

    refactor: Rediseñar sistema de shortcuts sin mayúsculas (WIP)
    CAMBIOS PRINCIPALES:
    - Git: Mover gpl->gl, gps->gp, gv->gb (blame más obvio)
    - Git: Reorganizar hunks h*->gs* (stage operations)
    - Git: Blame/Branches bajo gb*, History bajo gh*, Diff bajo gd*
    - Flutter: Cambiar F* mayúsculas a fl* minúsculas (más rápido)
    - Testing: Cambiar T* mayúsculas a te* minúsculas (sin Shift)
    
    BENEFICIOS:
    - 0 mayúsculas (sin Shift = más rápido)
    - Agrupación lógica por prefijo (autocompletado)
    - Cada comando único (0 duplicados)
    - Nombres intuitivos (comando = acción)
    
    Pendiente: LSP, documentación, ideavimrc sync

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -5,10 +5,10 @@ return {
     },
     config = function()
         -- Test commands: mantener solo los esenciales
-        vim.keymap.set("n", "<leader>T", ":TestNearest<CR>", { desc = "Test: Nearest" })
-        vim.keymap.set("n", "<leader>Tf", ":TestFile<CR>", { desc = "Test: File" })
-        vim.keymap.set("n", "<leader>Ta", ":TestSuite<CR>", { desc = "Test: All Suite" })
-        vim.keymap.set("n", "<leader>Tl", ":TestLast<CR>", { desc = "Test: Last" })
+        vim.keymap.set("n", "<leader>ten", ":TestNearest<CR>", { desc = "Test: Nearest" })
+        vim.keymap.set("n", "<leader>tenf", ":TestFile<CR>", { desc = "Test: File" })
+        vim.keymap.set("n", "<leader>tena", ":TestSuite<CR>", { desc = "Test: All Suite" })
+        vim.keymap.set("n", "<leader>tenl", ":TestLast<CR>", { desc = "Test: Last" })
         -- TestVisit eliminado por uso poco frecuente
         vim.cmd("let test#strategy = 'vimux'")
     end,
