1d23c035c6f310dc73ad40ebd11f45a0e89dcf07
ChochyLinux <ohchochy@gmail.com>
2026-04-10 17:21:27 -0600

    fix: Update treesitter to new API, fix dashboard header
    - Rewrite treesitter config for new nvim-treesitter API (the
      'nvim-treesitter.configs' module was removed in latest version).
      Use main="nvim-treesitter" with opts for parser installation.
    - Configure textobjects separately with pcall-protected keymaps
      for function/class/argument selection, movement, and swap.
    - Remove rainbow-delimiters dependency (incompatible with nvim 0.12)
    - Simplify CAPDESIS dashboard header for better rendering
    - Clean up dashboard footer format

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,72 +7,58 @@ return {
         dependencies = {
             "nvim-treesitter/nvim-treesitter-textobjects",
         },
+        -- New nvim-treesitter API (configs module was removed)
+        main = "nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "lua", "vim", "vimdoc", "query",
+                "dart", "kotlin", "c", "cpp", "go",
+                "python", "javascript", "typescript", "html", "css",
+                "json", "yaml", "markdown", "bash",
+            },
+            auto_install = false,
+        },
+    },
+    -- Text objects for functions, classes, arguments, etc.
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
         config = function()
-            local ts_configs = require("nvim-treesitter.configs")
-            ts_configs.setup({
-                ensure_installed = {
-                    "lua", "vim", "vimdoc", "query",
-                    "dart", "kotlin", "c", "cpp", "go",
-                    "python", "javascript", "typescript", "html", "css",
-                    "json", "yaml", "markdown", "bash",
-                    -- swift requires tree-sitter CLI: npm install -g tree-sitter-cli
-                },
-                sync_install = false,
-                auto_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
+            -- Use vim.treesitter built-in for textobjects (new API)
+            local ok, ts_repeat = pcall(require, "nvim-treesitter-textobjects.repeatable_move")
 
-                -- Text objects: select/move by function, class, parameter, etc.
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["af"] = { query = "@function.outer", desc = "Around function" },
-                            ["if"] = { query = "@function.inner", desc = "Inside function" },
-                            ["ac"] = { query = "@class.outer", desc = "Around class" },
-                            ["ic"] = { query = "@class.inner", desc = "Inside class" },
-                            ["aa"] = { query = "@parameter.outer", desc = "Around argument" },
-                            ["ia"] = { query = "@parameter.inner", desc = "Inside argument" },
-                            ["ai"] = { query = "@conditional.outer", desc = "Around conditional" },
-                            ["ii"] = { query = "@conditional.inner", desc = "Inside conditional" },
-                            ["al"] = { query = "@loop.outer", desc = "Around loop" },
-                            ["il"] = { query = "@loop.inner", desc = "Inside loop" },
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true,
-                        goto_next_start = {
-                            ["]f"] = { query = "@function.outer", desc = "Next function start" },
-                            ["]c"] = { query = "@class.outer", desc = "Next class start" },
-                            ["]a"] = { query = "@parameter.inner", desc = "Next argument" },
-                        },
-                        goto_next_end = {
-                            ["]F"] = { query = "@function.outer", desc = "Next function end" },
-                            ["]C"] = { query = "@class.outer", desc = "Next class end" },
-                        },
-                        goto_previous_start = {
-                            ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-                            ["[c"] = { query = "@class.outer", desc = "Previous class start" },
-                            ["[a"] = { query = "@parameter.inner", desc = "Previous argument" },
-                        },
-                        goto_previous_end = {
-                            ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-                            ["[C"] = { query = "@class.outer", desc = "Previous class end" },
-                        },
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ["<leader>sa"] = { query = "@parameter.inner", desc = "Swap with next argument" },
-                        },
-                        swap_previous = {
-                            ["<leader>sA"] = { query = "@parameter.inner", desc = "Swap with previous argument" },
-                        },
-                    },
-                },
-            })
+            -- Textobject selection keymaps
+            local select_ok, ts_select = pcall(require, "nvim-treesitter-textobjects.select")
+            if select_ok then
+                local map = vim.keymap.set
+                -- Around/inside function
+                map({ "x", "o" }, "af", function() ts_select.select_textobject("@function.outer", "textobjects") end, { desc = "Around function" })
+                map({ "x", "o" }, "if", function() ts_select.select_textobject("@function.inner", "textobjects") end, { desc = "Inside function" })
+                -- Around/inside class
+                map({ "x", "o" }, "ac", function() ts_select.select_textobject("@class.outer", "textobjects") end, { desc = "Around class" })
+                map({ "x", "o" }, "ic", function() ts_select.select_textobject("@class.inner", "textobjects") end, { desc = "Inside class" })
+                -- Around/inside argument
+                map({ "x", "o" }, "aa", function() ts_select.select_textobject("@parameter.outer", "textobjects") end, { desc = "Around argument" })
+                map({ "x", "o" }, "ia", function() ts_select.select_textobject("@parameter.inner", "textobjects") end, { desc = "Inside argument" })
+            end
+
+            -- Move keymaps
+            local move_ok, ts_move = pcall(require, "nvim-treesitter-textobjects.move")
+            if move_ok then
+                local map = vim.keymap.set
+                map({ "n", "x", "o" }, "]f", function() ts_move.goto_next_start("@function.outer", "textobjects") end, { desc = "Next function" })
+                map({ "n", "x", "o" }, "[f", function() ts_move.goto_previous_start("@function.outer", "textobjects") end, { desc = "Previous function" })
+                map({ "n", "x", "o" }, "]c", function() ts_move.goto_next_start("@class.outer", "textobjects") end, { desc = "Next class" })
+                map({ "n", "x", "o" }, "[c", function() ts_move.goto_previous_start("@class.outer", "textobjects") end, { desc = "Previous class" })
+            end
+
+            -- Swap keymaps
+            local swap_ok, ts_swap = pcall(require, "nvim-treesitter-textobjects.swap")
+            if swap_ok then
+                vim.keymap.set("n", "<leader>sa", function() ts_swap.swap_next("@parameter.inner", "textobjects") end, { desc = "Swap with next argument" })
+                vim.keymap.set("n", "<leader>sA", function() ts_swap.swap_previous("@parameter.inner", "textobjects") end, { desc = "Swap with previous argument" })
+            end
         end,
     },
 }
